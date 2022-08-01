0648c86ff5fec1e3c0465a3b7f1482c2c1e663fa
Pavel Kuznetsov <Technic47@gmail.com>
2022-08-01 14:10:04 +0300

    Task41 done

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+int count = 0;
+Console.WriteLine("Enter your number useing spacebar: ");
+
+int[] numbers = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0) count++;
+}
+
+Console.WriteLine($"Count of positive numbers is: {count}");
