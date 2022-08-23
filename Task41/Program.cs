7ebf0207ca08f32b5e61bce771701c618087eb3e
Pavel Kuznetsov <Technic47@gmail.com>
2022-08-23 13:08:41 +0300

    remarks

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,11 +1,16 @@
 ﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
 Посчитайте, сколько чисел больше 0 ввёл пользователь.
 */
-int count = 0;
+
 Console.WriteLine("Enter your numbers: ");
 
 int[] numbers = Array.ConvertAll(Console.ReadLine()!.Split(' ', ',', '.', '\t'), int.Parse); 
 
-for (int i = 0; i < numbers.Length; i++) if (numbers[i] > 0) count++;
+int CountPos (int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++) if (numbers[i] > 0) count++;
+    return count;
+}
 
-Console.WriteLine($"Count of positive numbers is: {count}");
+Console.WriteLine($"Count of positive numbers is: {CountPos(numbers)}");
